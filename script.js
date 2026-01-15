4aaec7715c64a2909b44313cff4ad98b1f06db66
Carmenchu <carmenxfdz@gmail.com>
2026-01-15 02:25:50 +0100

    Pomodoro finished

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const { ipcRenderer } = require('electron');
 
-const focusTime = 1*60;
-const breakTime = 0.5*60;
-const largeBreak = 0.75*60;
+const focusTime = 0.1*60;
+const breakTime = 0.1*60;
+const largeBreak = 15*60;
+
+const alarmSound = new Audio('assets/sound/timer.wav');
 
 const finishButtons = document.querySelectorAll('.finishButton');
 
@@ -15,15 +17,37 @@ const breakPhrases = [
 ]
 
 let time = 0;
-let intervalo;
+let intervalo = null;
 let focusCount = 0;
 let totalFocusTime = 0;
+let isPaused = false;
+let currentPhase = "focus"; // "focus" | "break" | "longBreak"
 
 
-console.log("JS cargado");
+const sakuraImg = document.getElementById("sakuraImg");
 
-const btn = document.getElementById("startButton");
-console.log("Botón:", btn);
+const totalFrames = 5;
+let currentFrame = 1;
+let animInterval = null;
+
+document.getElementById('pauseButton').style.backgroundImage = `url('assets/buttons/PauseButton.png')`;
+
+function startSakuraAnimation() {
+    if (animInterval) return;   // 🔒 evita duplicados 
+    animInterval = setInterval(() => {
+        currentFrame++;
+        if (currentFrame > totalFrames) currentFrame = 1;
+
+        sakuraImg.src = `assets/sakura/${String(currentFrame).padStart(2,"0")}.png`;
+    }, 200);
+}
+
+function stopSakuraAnimation(finalFrame = 5) {
+    clearInterval(animInterval);
+    animInterval = null;   // 🔥 importante
+    sakuraImg.src = `assets/sakura/${String(finalFrame).padStart(2,"0")}.png`;
+}
+
 
 function startTimer(){
     document.getElementById('StartPage').style.display = 'none';
@@ -33,48 +57,61 @@ function startTimer(){
     totalFocusTime = 0;
 
     document.getElementById("countdown").textContent =
-    `${Math.floor(focusTime / 60)}:${(focusTime % 60).toString().padStart(2, "0")}`;
+    `${Math.floor(focusTime / 60).toString().padStart(2, "0")}:${(focusTime % 60).toString().padStart(2, "0")}`;
 
     setTimeout(()=>{
+        currentFrame = 1;
+        startSakuraAnimation();
         Timer(focusTime);
+        currentPhase = "focus";
     }, 100);
 };
 
 function Timer(tiempo){
+    if (intervalo) return; // evita múltiples intervalos
+
     time = tiempo;
 
     intervalo = setInterval(() =>{
         time--;
         
-        if(tiempo == focusTime) totalFocusTime++;
+        if(currentPhase =="focus") totalFocusTime++;
 
         updateTime();
         if (time <= 0) {
+            alarmSound.currentTime = 0;  // reinicia si estaba sonando
+            alarmSound.play();
             clearInterval(intervalo);
-            switch(tiempo){
-                case focusTime: 
-                    document.getElementById('TimerPage').style.display = 'none';
-                    document.getElementById('BreakPage').style.display = 'flex';
-                    
-                    if(focusCount%4 != 0){
-                        document.getElementById("countdown").textContent =
-                        `${Math.floor(breakTime / 60)}:${(breakTime % 60).toString().padStart(2, "0")}`;
-                        setTimeout(()=>{
-                            Timer(breakTime);
-                        }, 100);
-                    }
-                    else{
-                        document.getElementById("countdown").textContent =
-                        `${Math.floor(largeBreak / 60)}:${(largeBreak % 60).toString().padStart(2, "0")}`;
-                        setTimeout(()=>{
-                            Timer(largeBreak);
-                        }, 100);
-                    }
-                    break;
-                case breakTime || largeBreak:
-                    document.getElementById('againButton').style.display = 'flex';
-                    document.getElementById('finishBreak').style.display = 'flex';
-                    break;
+            intervalo = null;
+            stopSakuraAnimation(finalFrame = 5);
+            if(currentPhase =="focus"){ 
+                document.getElementById('TimerPage').style.display = 'none';
+                document.getElementById('breakPhrase').textContent = breakPhrase();
+                document.getElementById('BreakPage').style.display = 'flex';
+                
+                if(focusCount%4 != 0){
+                    document.getElementById("countdown").textContent =
+                    `${Math.floor(breakTime / 60).toString().padStart(2, "0")}:${(breakTime % 60).toString().padStart(2, "0")}`;
+                    setTimeout(()=>{
+                        currentFrame = 1;
+                        startSakuraAnimation();
+                        currentPhase = "break";
+                        Timer(breakTime);
+                    }, 100);
+                }
+                else{
+                    document.getElementById("countdown").textContent =
+                    `${Math.floor(largeBreak / 60).toString().padStart(2, "0")}:${(largeBreak % 60).toString().padStart(2, "0")}`;
+                    setTimeout(()=>{
+                        currentFrame = 1;
+                        startSakuraAnimation();
+                        currentPhase = "longBreak";
+                        Timer(largeBreak);
+                    }, 100);
+                }
+            } else if(currentPhase =="break" || currentPhase =="longBreak"){
+                    document.getElementById('breakPhrase').style.display = 'none';
+                    document.getElementById('breakButtons').style.display = 'flex';
             }
         }
     }, 1000);
@@ -85,35 +122,56 @@ function updateTime() {
     const segundos = time % 60;
 
     document.getElementById("countdown").textContent =
-    `${minutos}:${segundos.toString().padStart(2, "0")}`;
+    `${minutos.toString().padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`;
+}
+
+
+
+function pauseTimer(){
+    if (!intervalo) return;
+
+    clearInterval(intervalo);
+    intervalo = null;
+    isPaused = true;
+    stopSakuraAnimation(currentFrame); // o frame de pausa
+}
+
+function resumeTimer() {
+    if (!isPaused) return;
+
+    isPaused = false;
+    startSakuraAnimation();
+    Timer(time); // seguimos con el tiempo restante
 }
 
 function breakPhrase(){
     const index = Math.floor(Math.random() * breakPhrases.length);
-    return breakPhrases[indice];
+    return breakPhrases[index];
 }
 
 function finishFocus(){
     document.getElementById('TimerPage').style.display = 'none';
     document.getElementById('sakura').style.display = 'none';
     document.getElementById('BreakPage').style.display = 'none';
+    document.getElementById('breakButtons').style.display = 'none';
+    document.getElementById('breakPhrase').style.display = 'flex';
     document.getElementById('FinishPage').style.display = 'flex';
+    clearInterval(intervalo);
+    intervalo = null
 
     const minutos = Math.floor(totalFocusTime / 60);
     const segundos = totalFocusTime % 60;
     document.getElementById("totalTime").textContent =
-    `${minutos} minutes and ${segundos.toString().padStart(2, "0")} seconds`;
+    `${minutos.toString().padStart(2, "0")} minutes and ${segundos.toString().padStart(2, "0")} seconds`;
 }
 
 
 document.getElementById("startButton").addEventListener('click', () => {
-    console.log("empezado");
     startTimer();
 });
 
 finishButtons.forEach(button => {
     button.addEventListener('click', () =>{
-        console.log("terminado");
         finishFocus();
     });
 });
@@ -121,32 +179,38 @@ finishButtons.forEach(button => {
 document.getElementById('againButton').addEventListener('click', () => {
     document.getElementById('TimerPage').style.display = 'flex';
     document.getElementById('BreakPage').style.display = 'none';
-    console.log("again");
+    document.getElementById('breakPhrase').style.display = 'flex';
+    document.getElementById('breakButtons').style.display = 'none';
     focusCount++;
     setTimeout(()=>{
+        currentFrame = 1;
+        startSakuraAnimation();
+        currentPhase = "focus";
         Timer(focusTime);
     }, 100);
 });
 
 document.getElementById('pauseButton').addEventListener('click', () => {
-    console.log("pause");
-
+    if (intervalo) {
+        pauseTimer();
+        document.getElementById('pauseButton').style.backgroundImage = `url('assets/buttons/ContinueButton.png')`;
+    } else {
+        resumeTimer();
+        document.getElementById('pauseButton').style.backgroundImage = `url('assets/buttons/PauseButton.png')`;
+    }
 });
 
 document.getElementById('homeButton').addEventListener('click', () => {
-    console.log("home");
     document.getElementById('StartPage').style.display = 'flex';
     document.getElementById('sakura').style.display = 'none';
     document.getElementById('FinishPage').style.display = 'none';
 });
 
 document.getElementById('closeButton').addEventListener('click', () => {
-    console.log("close");
     ipcRenderer.send('close-app');
 });
 
 document.getElementById('minimizeButton').addEventListener('click', () => {
-    console.log("minimize");
     ipcRenderer.send('minimize-app');
 });
 
